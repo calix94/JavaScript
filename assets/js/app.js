7f61932106d251e235c4a9904ed9899d24cfe90f
Maynor Calix <calix2007@hotmail.com>
2020-07-13 22:31:28 -0600

    commit inicial de curso

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+
+// alert('hola desde app.js');
+
+// console.log(console.log('hola mundo'));
+
+let a = 10,
+    b = 20,
+    c = 'hola',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+
+
+// console.log('%c Mis Variables', 'color:blue; font-weight:bold')
+// console.log('{a}')
+// console.log('{b}')
+// console.log('{c}')
+
+
+c = 'Hola de nuevo';
+
+
+// console.table({a,b,c,d,x});
+
+var miNombre='Maynor';
